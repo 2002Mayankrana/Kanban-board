a6e24d34c0035366dc8747309a2b8557fe96db8b
2002Mayankrana <118000661+2002Mayankrana@users.noreply.github.com>
2024-01-06 11:34:51 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import "../styles/Card.css";
 
 const Card = ({ id, title, tags, status }) => {
   return (
-    <div className="container">
+    <div className="container" style={{ backgroundColor: "white"}}>
       <div className="cardHeading2" style={{ justifyContent: "space-between", backgroundColor: "white"}}>
         <span style={{ textTransform: "uppercase", color: "darkgrey" }}>
           {id}
